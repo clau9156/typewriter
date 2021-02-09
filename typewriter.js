d07e1537de385571ac3c793c2dba56aeddb457f7
clau9156 <clau9156@stud.kea.dk>
2021-02-09 20:04:22 +0100

    window.addeventlistener

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,50 @@
 "use strict";
+
+// init();
+window.addEventListener("load", init);
+
+// text in HTML
+let text = document.querySelector(".typewritten");
+// increment N
+let increment = 0;
+
+
+function init() {
+    console.log("init");
+    // get the text from somewhere
+    document.querySelector(".textwritten").textContent;
+
+    // clear the HTML area
+    document.querySelector(".textwritten").textContent = "";
+
+    // start loop
+    loop();
+}
+
+funcion loop() {
+    console.log("loop");
+    // show the N'th letter:
+    // - set textContent to subsring of 0 to N
+    if (increment < text.length) {
+        document.querySelector(".textwritten").textContent += text.charAt(increment);
+        // increment N
+        increment++;
+        // wait before calling loop () again
+        setTimeout(loop, 500); 
+    } 
+    // else{
+    // end! 
+    // }
+}
+
+
+// NOTES:
+// text = "text inside"
+// letters = text.substring(0,3) 
+// thi
+// document.querySelector(".typewritten").textContent = letters
+// thi
+// .substring()
+
+// .length
+// reminds me of password instead of * its just saying for every letter it adds another.
